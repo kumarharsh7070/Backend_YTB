bbe7b041e67605ce17a87df6f11d6bea6f9446d2
kumar harsh <hk995814@gmail.com>
2025-09-26 23:14:42 +0530

    register and validation

--- src/Controller/User.controller.js
+++ src/Controller/User.controller.js
@@ -1,9 +1,29 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import { User } from "../models/user.model.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "User registered successfully",
-  });
-});
+ const {fullName, email, username, password}= req.body
+ 
+ if(!fullName || !email || !username || !password){
+    throw new ApiError("All fields are required",400)
+ }
+  const emailRegex = /\S+@\S+\.\S+/; //validate email format
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ message: "Invalid email format" });
+  }
+
+  const extistedUser = User.findOne({
+    $or:[{ username }, { email }]
+  })
+  if(extistedUser){
+    throw new ApiError("User already exists",409)
+  }
+
+ const avatarLocalPath= req.files?.avatar[0]?.path
+ console.log(avatarLocalPath);
+ const coverLocalPath=req.files?.coverimage[0]?.path
+ console.log(coverLocalPath);
+ });
 
 export { registerUser };
