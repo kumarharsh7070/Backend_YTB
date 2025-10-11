0c12d5ed98a84dd4ae138ee601aba840f7c3128d
kumar harsh <hk995814@gmail.com>
2025-10-11 15:23:14 +0530

    end points and pipeline

--- src/Controller/User.controller.js
+++ src/Controller/User.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshToken = async(userId) =>{
      try {
@@ -130,14 +131,14 @@ const logoutUser = asyncHandler(async(req, res) =>{
 
 const refreshAccessToken =asyncHandler(async(req,res)=>{
 
-   const incomingRefresToken = req.cookies.refreshToken || req.body.refreshToken
-   if(!incomingRefresToken){
+   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+   if(!incomingRefreshToken){
     throw new ApiError (401,"unauthroize request")
    }
 
   try {
     const decodedToken = jwt.verify(
-      incomingRefresToken,
+      incomingRefreshToken,
       process.env.REFRESH_TOKEN_SECRET
      )
     const user = await User.findById(decodedToken?._id)
@@ -145,7 +146,7 @@ const refreshAccessToken =asyncHandler(async(req,res)=>{
       throw new ApiError(401,"invalid refresh token")
     }
   
-    if(incomingRefresToken !== user?.refreshToken){
+    if(incomingRefreshToken !== user?.refreshToken){
        throw new ApiError(401, "Invalid refresh token")
     }
   
@@ -206,8 +207,8 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
         req.user?._id,
         {
             $set:{
-                fullName:email,
-                email:email
+                fullName: fullName,
+               email: email.toLowerCase()
             }
         },
         {new:true}
@@ -355,6 +356,62 @@ const getUserChannelProfile =asyncHandler(async (req,res)=>{
         new ApiResponse(200, channel[0], "User channel fetched successfully")
     )
 })
+
+const getWatchHistory = asyncHandler(async(req, res)=>{
+     const user = await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user._id)
+            },
+           
+        },
+        {
+            $lookup:{
+                from:"video",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"user",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1
+
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+     ])
+
+     return res
+     .status(200)
+     .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "watch history fetched successfully"
+        )
+     )
+})
 export { 
     registerUser,
     loginUser,
@@ -365,5 +422,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage ,
-getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 };
