76c23c44b1089538cf4e05073d25379cf9c96f20
kumar harsh <hk995814@gmail.com>
2026-01-11 22:50:17 +0530

    Fix case-sensitive imports for Vercel build

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Login from "./pages/login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
