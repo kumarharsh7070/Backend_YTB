a2a97aaabf892c14d4f1a292325932b1e1c2ab7d
kumar harsh <hk995814@gmail.com>
2026-01-11 22:01:32 +0530

    Fix frontend env and prepare for deployment

--- fronted/src/api/axios.js
+++ fronted/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1", // backend base URL
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 // Attach token automatically
